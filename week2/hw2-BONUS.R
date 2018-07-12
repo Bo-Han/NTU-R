b024103552479ef58df8045e785bf7b05296b2e2
Bo-Han <b06208013@ntu.edu.tw>
2018-07-12 12:47:28 +0800

    modify

--- week2/hw2-BONUS.R
+++ week2/hw2-BONUS.R
@@ -6,4 +6,4 @@ map <- get_map(location = c(lon = 121.555, lat = 25.0592), zoom = 12,language = 
 
 
 u <- read.csv("Address_Finish.csv")
-ggmap(map) + geom_point(aes(x = Response_X, y = Response_Y ), data = u )
+ggmap(map,darken = c(0.2, "white")) + geom_point(aes(x = Response_X, y = Response_Y ), data = u, color="purple", size=0.9 )+labs(title = "台北市102年到107年自行車竊盜分布地圖")
